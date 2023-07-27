630123b4ad31fe6950db7b2741d0caf38a971ba3
Alexander Städing <alexanderstaeding@gmail.com>
2023-07-27 12:29:11 +0200

    Make plots for 01_put_coalescing

--- benchmarks/01_put_coalescing/bench.sh
+++ benchmarks/01_put_coalescing/bench.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p results
-
-elements_list=(100 200 400 800 1600 3200 6400 12800 25600 51200 102400 204800 409600)
+#elements_list=(100 200 400 800 1600 3200 6400 12800 25600 51200 102400 204800 409600)
+elements_list=(100 200)
 grids_list=(1 2 4 8 16 32 64)
 blocks_list=(1 2 4 8 16 32 64)
 
@@ -14,7 +13,7 @@ for elem in "${elements_list[@]}"; do
   for grid in "${grids_list[@]}"; do
     for block in "${blocks_list[@]}"; do
       echo "Running with 1 host (2 PEs) num_elements=$elem, num_blocks=$block"
-      echo -n "01_put_coalescing,1,$elem,100,1,$block" >>$output_file
+      echo -n "01_put_coalescing,1,$elem,100,$grid,$block" >>$output_file
       nvshmrun -np 2 ./bench_01_put_coalescing $elem 100 $grid $block >>$output_file
     done
   done
@@ -24,7 +23,7 @@ for elem in "${elements_list[@]}"; do
   for grid in "${grids_list[@]}"; do
     for block in "${blocks_list[@]}"; do
       echo "Running with 2 hosts (2 PEs) num_elements=$elem, num_blocks=$block"
-      echo -n "01_put_coalescing,2,$elem,100,1,$block" >>$output_file
+      echo -n "01_put_coalescing,2,$elem,100,$grid,$block" >>$output_file
       nvshmrun -n 2 -ppn 2 --hosts 10.0.2.11,10.0.2.12 ./bench_01_put_coalescing $elem 100 $grid $block >>$output_file
     done
   done

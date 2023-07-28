719f863b34b3e90856b3222a4f076c17924d1d73
Jonas Weßner <jonaswessner@gmx.de>
2023-07-28 12:00:59 +0200

    #12 extract Meas struct for bench 01

--- benchmarks/01_put_coalescing/nvshmem_put_coalescing.cu
+++ benchmarks/01_put_coalescing/nvshmem_put_coalescing.cu
@@ -5,6 +5,7 @@
 #include <cuda.h>
 #include <c++/10/array>
 #include "nvshmem.h"
+#include "Meas.cuh"
 
 // used to check the status code of cuda routines for errors
 #undef CUDA_CHECK
@@ -28,44 +29,6 @@
             exit(-1);                                                                      \
         }                                                                                  \
     } while (0)
-
-
-constexpr long long SHADER_FREQ_KHZ{1530};
-constexpr long long SHADER_FREQ_HZ{1530000};
-
-struct Meas {
-    long long start = 0;
-    long long stop = 0;
-
-    __host__  [[nodiscard]] inline long long clock_diff() const {
-        return stop - start;
-    }
-
-    __host__  [[nodiscard]] long double time_diff_ms() const {
-        return static_cast<long double>(this->clock_diff()) / SHADER_FREQ_KHZ;
-    }
-
-    __host__  [[nodiscard]] long double time_diff_s() const {
-        return static_cast<long double>(this->clock_diff()) / SHADER_FREQ_HZ;
-    }
-
-    __host__ [[nodiscard]] long double get_throughput(const uint64_t n_bytes) const {
-        return n_bytes / this->time_diff_s() / 1000000;
-    }
-
-    __host__ [[nodiscard]] std::string to_csv() const {
-        // TODO: @Alex If you want, use this to implement printing to csv format
-        return {};
-    }
-
-    __host__ std::string to_string(const uint64_t n_bytes) const {
-        return "start=" + std::to_string(start) +
-               " stop=" + std::to_string(stop) +
-               " clock_diff=" + std::to_string(this->clock_diff()) +
-               " (" + std::to_string(this->time_diff_ms()) +
-               "ms) throughput=" + std::to_string(this->get_throughput(n_bytes)) + " GB/s";
-    }
-};
 
 // TODO: verify results make sense
 // TODO: return results to CPU and print in csv format

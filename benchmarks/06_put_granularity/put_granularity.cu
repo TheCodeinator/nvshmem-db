97ee0f34d378aba94559f3a095b38c18ee23f88c
Alexander Städing <alexanderstaeding@gmail.com>
2023-08-20 11:27:35 +0200

    Rewrite 06_put_granularity

--- benchmarks/06_put_granularity/put_granularity.cu
+++ benchmarks/06_put_granularity/put_granularity.cu
@@ -4,51 +4,42 @@
 #include <chrono>
 #include <cuda.h>
 #include <vector>
+#include <bit>
 #include "nvshmem.h"
 #include "NVSHMEMUtils.cuh"
 
-constexpr long long MAX_SEND_SIZE{1024 * 1024 * 1024};
-constexpr size_t N_TESTS{log2const(MAX_SEND_SIZE) + 1};
+
 
 __global__ void generalized_benchmark(uint8_t *data_source,
                                       uint8_t *data_sink,
                                       const int this_pe,
                                       const uint64_t num_bytes,
-                                      const uint32_t size_per_element) {
+                                      const uint64_t message_size,
+                                      const uint64_t max_send_size) {
     if (this_pe != 0) {
+        // wait to receive data from PE 0
         nvshmem_barrier_all();
         return;
     }
-    const uint32_t thread_id = global_thread_id();
-    const uint32_t size_per_thread = size_per_element / global_thread_count();
-    const uint32_t thread_offset = thread_id * size_per_thread;
+    const uint64_t thread_id = global_thread_id();
+    const uint64_t thread_offset = thread_id * message_size;
 
-    // data_size 1x10^9
-    // num_bytes 512
-    // size_per_element 64
-
-    // print all variables in one line
-//    printf("thread_id %u, thread_offset %u, size_per_thread %u, size_per_element %u, num_bytes %lu\n",
-//           thread_id, thread_offset, size_per_thread, size_per_element, num_bytes);
-
-    for (size_t i = 0; i < num_bytes / size_per_element; i += size_per_element) {
-        assert(i * size_per_element + thread_offset < MAX_SEND_SIZE);
+    for (uint64_t i = 0; i < num_bytes / max_send_size; ++i) {
         nvshmem_uint8_put_nbi(
-                data_sink + i * size_per_element + thread_offset,
-                data_source + i * size_per_element + thread_offset,
-                size_per_element,
-                1);
+            data_sink + thread_offset,
+            data_source + thread_offset,
+            message_size,
+            1);
     }
 
     nvshmem_quiet();
+    // synchronize all PEs (notify PE 1 that data is finished)
     nvshmem_barrier_all();
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        std::cerr << "Usage: " << argv[0] << " <grid_dims> <block_dims> <num_hosts> <size_per_element>"
-                  << std::endl;
-        return 1;
+    if (argc != 6) {
+        throw std::invalid_argument("Usage: " + std::string(argv[0]) + " <grid_dims> <block_dims> <num_hosts> <num_bytes> <max_send_size>");
     }
 
     int n_pes, this_pe;
@@ -56,7 +47,29 @@ int main(int argc, char *argv[]) {
 
     const uint32_t grid_dim = std::stoi(argv[1]);
     const uint32_t block_dim = std::stoi(argv[2]);
-    const uint32_t size_per_element = std::stoi(argv[4]);
+    const uint32_t num_hosts = std::stoi(argv[3]);
+
+    // the number of bytes that are sent in total per kernel
+    const uint64_t num_bytes = std::stoi(argv[4]);
+
+    // the maximum number of bytes that are sent with a single nvshmem put call (increases in powers of 2 starting at 1)
+    const uint64_t max_send_size = std::stoi(argv[5]);
+
+    const uint64_t buffer_size = grid_dim * block_dim * max_send_size;
+
+    if (std::popcount(max_send_size) != 1) {
+        throw std::invalid_argument("max_send_size must be a power of 2");
+    }
+
+    if (num_bytes / (grid_dim * block_dim * max_send_size) < 1) {
+        throw std::invalid_argument(
+            "num_bytes must be greater than grid_dim * block_dim * max_send_size (= " + std::to_string(buffer_size) + ")");
+    }
+
+    if (num_bytes % (grid_dim * block_dim * max_send_size) != 0) {
+        throw std::invalid_argument(
+            "num_bytes must be a multiple of grid_dim * block_dim * max_send_size (= " + std::to_string(buffer_size) + ")");
+    }
 
     nvshmem_init();
     this_pe = nvshmem_team_my_pe(NVSHMEM_TEAM_WORLD);
@@ -69,22 +82,19 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    auto data_size = MAX_SEND_SIZE * block_dim * grid_dim;
-    size_t *data_source;
-    cudaMalloc(&data_source, data_size);
-    auto *data_sink = static_cast<uint8_t *>(nvshmem_malloc(data_size));
+    // each thread is allocated one MAX_SEND_SIZE element, which is re-sent until num_bytes are sent
+    auto *data_source = static_cast<uint8_t *>(nvshmem_malloc(buffer_size));
+    auto *data_sink = static_cast<uint8_t *>(nvshmem_malloc(buffer_size));
 
-    for (size_t test = 0; test < N_TESTS; ++test) {
-        const auto msg_size = int_pow(2, test);
+    for (uint64_t message_size = 0; message_size < max_send_size; message_size <<= 1) {
         const auto time_taken = time_kernel(generalized_benchmark, grid_dim, block_dim, 0, stream,
-                                            data_source, data_sink, this_pe, msg_size, size_per_element);
-        nvshmem_quiet();
+                                            data_source, data_sink, this_pe, num_bytes, message_size, max_send_size);
         if (this_pe == 0) {
-            std::cout << msg_size << " bytes took " << time_taken.count() << " microseconds." << std::endl;
+            std::cout << message_size << " bytes took " << time_taken.count() << " microseconds." << std::endl;
         }
     }
 
-    cudaFree(data_source);
+    nvshmem_free(data_source);
     nvshmem_free(data_sink);
     nvshmem_finalize();
     return 0;

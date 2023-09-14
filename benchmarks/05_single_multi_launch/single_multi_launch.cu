3846ece799351d2cd41fbc906ce8f0d7a550fed6
Luis Wientgens <windgans@protonmail.com>
2023-09-14 20:49:04 +0200

    Use correct duration for time2, revert to single memory region

--- benchmarks/05_single_multi_launch/single_multi_launch.cu
+++ benchmarks/05_single_multi_launch/single_multi_launch.cu
@@ -23,15 +23,15 @@ __global__ void calculate(const uint32_t * in, uint32_t * buff, size_t size_buff
 /*
     Emulate calculation of sending destination and sending to remotes
 */
-__global__ void calculate_and_send(const uint32_t* in, uint32_t* buff, uint32_t size_in,
-                                   uint32_t size_buf, uint32_t * sym_mem, uint32_t this_pe){
+__global__ void calculate_and_send(const uint32_t* in, uint32_t size_in,
+                                   uint32_t size_buff, uint32_t * sym_mem, uint32_t this_pe){
 
-    for(uint32_t off{0}; off<size_in; off+=size_buf){
+    for(uint32_t off{0}; off<size_in; off+=size_buff){
 
         // Compute capability >= 7.0 (V100)
         __nanosleep(1e9);
-
-        nvshmem_uint32_put_nbi(sym_mem, in+off, size_buf, 1-this_pe);
+        memcpy(sym_mem+off,in+off,size_buff*sizeof(uint32_t));
+        nvshmem_uint32_put_nbi(sym_mem, sym_mem+size_in+off, size_buff, 1-this_pe);
 
     }
 
@@ -80,61 +80,65 @@ int main(int argc, char* argv[]){
     CUDA_CHECK(cudaMemset(in, 1, size_in*sizeof(uint32_t)));
 
     size_t size_buff = 4096;
-    uint32_t * buff1;
-    uint32_t * buff2;
-    CUDA_CHECK(cudaMalloc((void**)&buff1, 2*size_buff*sizeof(uint32_t)));
-    CUDA_CHECK(cudaMalloc((void**)&buff2, 2*size_buff*sizeof(uint32_t)));
 
-    uint32_t * sym_mem = reinterpret_cast<uint32_t *>(nvshmem_malloc(size_in*sizeof(uint32_t)));
+    // for use with multiple memory regions
+    //uint32_t * buff1;
+    //uint32_t * buff2;
+    //CUDA_CHECK(cudaMalloc((void**)&buff1, 2*size_buff*sizeof(uint32_t)));
+    //CUDA_CHECK(cudaMalloc((void**)&buff2, 2*size_buff*sizeof(uint32_t)));
+
+    uint32_t * buff;
+    CUDA_CHECK(cudaMalloc((void**)&buff, 4*size_buff*sizeof(uint32_t)));
+
+    uint32_t * sym_mem = reinterpret_cast<uint32_t *>(nvshmem_malloc(2*size_in*sizeof(uint32_t)));
 
     // Make RDMA connection
     rdma::RDMA server {my_ip, rdma_port};
 
     // register RDMA memory
+
+    // not yet supported
     // memory region1
-    server.register_memory(buff1, 2*size_buff*sizeof(uint32_t));
+    //server.register_memory(buff1, 2*size_buff*sizeof(uint32_t));
     // memory region2
-    server.register_memory(buff2, 2*size_buff*sizeof(uint32_t));
+    //server.register_memory(buff2, 2*size_buff*sizeof(uint32_t));
+
+    server.register_memory(buff, 4*size_buff*sizeof(uint32_t));
 
     server.listen(rdma::RDMA::CLOSE_AFTER_LAST | rdma::RDMA::IN_BACKGROUND);
 
     // wait for discovery
-    sleep(1);
+    sleep(10);
 
     rdma::Connection* conn = server.connect_to(other_ip,rdma_port);
 
     // Warm up CUDA context
-    calculate<<<1,1>>>(in, buff1, size_buff);
+    calculate<<<1,1>>>(in, buff, size_buff);
 
-    auto start = std::chrono::steady_clock::now();
+    auto dur = time_kernel(calculate_and_send, 1, 1, size_buff, stream1,
+                in, size_in, size_buff, sym_mem, this_pe);
 
-    time_kernel(calculate_and_send, 1, 1, size_buff, stream1, in, buff1, size_in, size_buff, sym_mem, this_pe);
-    cudaStreamSynchronize(stream1);
-
-    auto stop = std::chrono::steady_clock::now();
-
-    auto dur = stop-start;
     auto t_ms = dur.count()*1e-6;
 
     auto start2 = std::chrono::steady_clock::now();
 
-
+    // mr_id only important with buff in different memory regions
     for(auto i{0}; i<size_in; i+=2*size_buff){
-        calculate<<<1,1,0,stream1>>>(in+i, buff1, size_buff);
+        calculate<<<1,1,0,stream1>>>(in+i, buff, size_buff);
         cudaStreamSynchronize(stream1);
-        conn->write(buff1,
+        conn->write(buff,
                     size_buff*sizeof(uint32_t),
-                    size_buff*sizeof(uint32_t),
+                    2*size_buff*sizeof(uint32_t),
                     rdma::Flags().signaled(),
                     0);
         if(i!=0) conn->sync_signaled(1);
-        calculate<<<1,1,0,stream2>>>(in+i+size_buff, buff2, size_buff);
+        calculate<<<1,1,0,stream2>>>(in+i+size_buff, buff+size_buff, size_buff);
         cudaStreamSynchronize(stream2);
-        conn->write(buff2,
+        conn->write(buff+size_buff,
                     size_buff*sizeof(uint32_t),
-                    size_buff*sizeof(uint32_t),
+                    3*size_buff*sizeof(uint32_t),
                     rdma::Flags().signaled(),
-                    1);
+                    0);
         conn->sync_signaled(1);
     }
 
@@ -145,7 +149,7 @@ int main(int argc, char* argv[]){
     server.close(conn);
 
     auto dur2 = stop2-start2;
-    auto t_ms2 = dur.count()*1e-6;
+    auto t_ms2 = dur2.count()*1e-6;
 
     auto num_launches = size_in/size_buff;
 

9593aa44a1033e2cf442a5c79e395fef487aff8b
Jonas Weßner <jonaswessner@gmx.de>
2023-09-19 16:14:18 +0800

    #0 change plot xaxis labels

--- benchmarks/tools/09_gpu_cpu_st.py
+++ benchmarks/tools/09_gpu_cpu_st.py
@@ -15,14 +15,28 @@ data['log2_message_size'] = np.log2(data['message_size'])
 # Create the plot
 plt.figure(figsize=(10, 6))
 
+# Round message_size to the nearest power of 2
+rounded_message_sizes = [2**int(np.round(np.log2(size))) for size in cpu_data['message_size']]
+
 # Plot CPU-driven data
-plt.semilogx(data[data['is_gpu'] == 0]['message_size'], cpu_data['throughput'], label='CPU-driven', color='blue', marker='o')
+plt.semilogx(rounded_message_sizes, cpu_data['throughput'], label='CPU-driven', color='blue', marker='o')
 
 # Plot nvshmem data
-plt.semilogx(data[data['is_gpu'] == 1]['message_size'], nvshmem_data['throughput'], label='nvshmem', color='red', marker='s')
+plt.semilogx(rounded_message_sizes, nvshmem_data['throughput'], label='nvshmem', color='red', marker='s')
 
-# Set x-axis labels as 2^log2(message_size)
-plt.xticks(data['message_size'], labels=['$2^{%d}$' % x for x in data['log2_message_size']], rotation=45)
+# Set x-axis labels as powers of 2 bytes, MB, or GB
+x_tick_locations = np.unique(rounded_message_sizes)
+x_labels = []
+
+for size in x_tick_locations:
+    if size < 1024:
+        x_labels.append(f'{int(size)}B')
+    elif size < 1024 * 1024:
+        x_labels.append(f'{int(size / 1024)}KB')
+    else:
+        x_labels.append(f'{int(size / (1024 * 1024))}GB')
+
+plt.xticks(x_tick_locations, labels=x_labels, rotation=45)
 
 # Set axis labels and legend
 plt.xlabel('Message Size')
